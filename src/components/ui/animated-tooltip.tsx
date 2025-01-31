f5224ae1a88f06a6522797a2d605df1b8158a110
asrar211 <113281948+asrar211@users.noreply.github.com>
2025-01-31 19:20:49 +0530

    image

--- src/components/ui/animated-tooltip.tsx
+++ src/components/ui/animated-tooltip.tsx
@@ -1,4 +1,6 @@
 "use client";
+import Image from "../../assets/icon.png"
+
 import { useState } from "react";
 import {
   motion,
@@ -80,9 +82,9 @@ export const AnimatedTooltip = ({
             onMouseMove={handleMouseMove}
             height={100}
             width={100}
-            src={item.image}
+            src={Image}
             alt={item.name}
-            className="object-cover !m-0 !p-0 object-top rounded-full h-14 w-14 border-2 group-hover:scale-105 group-hover:z-30 border-white  relative transition duration-500"
+            className="bg-white object-cover !m-0 !p-0 object-top rounded-full h-14 w-14 border-2 group-hover:scale-105 group-hover:z-30 border-red-400  relative transition duration-500"
           />
         </div>
       ))}
